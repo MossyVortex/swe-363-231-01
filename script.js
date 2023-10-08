5a7ed2b8e44fd97644ab842b4e3dbced8962b827
MOSA ALSHAKHOURI <67518753+MossyVortex@users.noreply.github.com>
2023-10-08 23:49:17 +0300

    JS2-Exercise2(A)

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+window.addEventListener("load", () => {
+
+document.querySelector(".loader").classList.add("loader--hidden");
+
+});
+
+const popup = document.getElementById('popupContainer');
+
+popup.style.display = 'none';
+
+function showPopup(){
+	
+	popup.style.display = 'flex';
+	
+}
+
+function closePopup(){
+	
+	popup.style.display = 'none';
+	
+}
